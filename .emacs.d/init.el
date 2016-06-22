95680fe5f5bc6c121d4cb4dec25d2045691149cf
Preet Bhinder <preet@apcera.com>
2016-06-21 20:57:47 -0700

    Change the location of backup files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -210,3 +210,11 @@
   )
 
 (global-set-key [f5] 'refresh-file)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
