ec2da3b7204d4e5733beacc41115aca78a287d55
Preet Bhinder <preet@apcera.com>
2016-06-21 20:57:32 -0700

    Renamed org files for agenda

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -158,12 +158,12 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(setq org-agenda-files (list "~/Dropbox/org/work.org"
-                             "~/Dropbox/org/home.org"))
-(setq org-src-fontify-natively t)
-(setq org-default-notes-file "~/Dropbox/org/inbox.org")
-(define-key global-map "\C-cc" 'org-capture)
 
+(setq org-agenda-files (list "~/Dropbox/org/"))
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
+(setq org-src-fontify-natively t)
+(setq org-default-notes-file "~/Dropbox/org/misc.org")
+(define-key global-map "\C-cc" 'org-capture)
 (setq org-todo-keyword-faces
       `(("TODO" . (:foreground "blue" :weight bold))
       ("IN-PROGRESS" . (:foreground "yellow" :weight bold))
